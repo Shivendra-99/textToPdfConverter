1298dd2aa16878aeb3658abc425e0468aa77c5a6
Shivendra Kumar Sonkar <sanusonkar1@gmail.com>
2025-01-03 10:16:54 +0530

    fix: the pdf upload path

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -95,8 +95,8 @@ public class App implements RequestHandler<S3Event, Map<String, Object>> {
 
             // Define the new object key for the PDF
             String pdfKey = objectKey.replace(".txt", ".pdf");
-             pdfKey = objectKey.replace("input/", "output/");
-            context.getLogger().log("Uploading the PDF to S3");
+             pdfKey = pdfKey.replace("input/", "output/");
+            context.getLogger().log("Uploading the PDF to S3: " + pdfKey);
 
             // Upload the PDF back to S3 with content length
             byte[] pdfBytes = pdfOutputStream.toByteArray();
