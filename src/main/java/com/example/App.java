ffc7a1939e73d92ac0f6b205257b96c5a36c691e
Shivendra Kumar Sonkar <sanusonkar1@gmail.com>
2025-01-04 00:54:13 +0530

    updated code to make call for AWS elastic beanstalk

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,213 +3,36 @@ package com.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfCopy;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.pdf.parser.PdfTextExtractor;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-public class App implements RequestHandler<S3Event, Map<String, Object>> {
-
-    private final AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-    private final ObjectMapper objectMapper;
-
-    public App() {
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JodaModule());
-    }
-
+public class App implements RequestHandler<S3Event, String> {
     @Override
-    public Map<String, Object> handleRequest(S3Event event, Context context) {
-        Map<String, Object> response = new HashMap<>();
-        try {
-            // Log the event details
-            String eventJson = objectMapper.writeValueAsString(event);
-            context.getLogger().log("Received event: " + eventJson);
-
+    public String handleRequest(S3Event event, Context context) {
+            context.getLogger().log("Received event: " + event);
             // Get the S3 event details
             String bucketName = event.getRecords().get(0).getS3().getBucket().getName();
             String objectKey = event.getRecords().get(0).getS3().getObject().getKey();
+            context.getLogger().log("Bucket: " + bucketName + ", Object: " + objectKey);
 
-            // Decode the object key
-            objectKey = URLDecoder.decode(objectKey, StandardCharsets.UTF_8.name());
+            context.getLogger().log("Making call to elastic beanstalk");
 
-            context.getLogger().log("Bucket name: " + bucketName);
-            context.getLogger().log("Object key: " + objectKey);
-
-            // List the objects in the bucket
-            ListObjectsV2Request listObjectsV2Request = new ListObjectsV2Request().withBucketName(bucketName)
-                    .withPrefix("output/");
-            ListObjectsV2Result result = s3Client.listObjectsV2(listObjectsV2Request);
-            // Will get the list of keys in the bucket
-            List<String> keys = result.getObjectSummaries().stream().map(s3ObjectSummary -> s3ObjectSummary.getKey())
-                    .collect(Collectors.toList());
-
-            for (String key : keys) {
-                if(key.equals("output/")) {
-                    continue;
-                }
-                context.getLogger().log("Key: " + key);
-                String KeyContains=key.split("/")[1];
-                String objectKeyContains=objectKey.split("/")[1];
-
-                if (KeyContains.startsWith(objectKeyContains.substring(0, 3))) {
-                    return updateThePDFFile(key, context, bucketName, objectKey, response);
-                }
+            String url = "http://texttopdfconverstion-env-1.eba-fm93rxki.us-east-1.elasticbeanstalk.com/?bucketName=" + bucketName + "&objectKey=" + objectKey;
+            try{
+                CloseableHttpClient client = HttpClients.createDefault();
+                HttpPost httpPost = new HttpPost(url);
+                String jsonString = "{\"bucketName\":\"" + bucketName + "\",\"objectKey\":\"" + objectKey + "\"}";
+                httpPost.setEntity(new StringEntity(jsonString));
+                httpPost.setHeader("Accept", "application/json");
+                httpPost.setHeader("Content-type", "application/json");
+                client.execute(httpPost);
             }
-
-            // Download the file from S3
-            context.getLogger().log("Starting the download of the file from S3");
-            S3Object s3Object = s3Client.getObject(bucketName, objectKey);
-            S3ObjectInputStream s3InputStream = s3Object.getObjectContent();
-           BufferedReader reader = new BufferedReader(new InputStreamReader(s3InputStream, StandardCharsets.UTF_8));
-            String line;
-            StringBuilder batchContent = new StringBuilder();
-            int batchSize = 100; // Number of lines to batch together
-            int lineCount = 0;
+            catch(Exception e){
+                context.getLogger().log("Error in making call to elastic beanstalk");
+            }
            
-
-            context.getLogger().log("Starting the conversion of the file to PDF");
-
-            // Create a PDF from the text content
-            Document document = new Document();
-            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
-            PdfWriter writer = PdfWriter.getInstance(document, pdfOutputStream);
-            document.open();
-            while ((line = reader.readLine()) != null) {
-                batchContent.append(line).append("\n");
-                lineCount++;
-                if (lineCount >= batchSize) {
-                    document.add(new Paragraph(batchContent.toString()));
-                    batchContent.setLength(0); // Clear the batch content
-                    lineCount = 0;
-                }
-            }
-            // Add any remaining content
-            if (batchContent.length() > 0) {
-                document.add(new Paragraph(batchContent.toString()));
-            }
-            reader.close();
-            document.close();
-            writer.close();
-
-            context.getLogger().log("PDF conversion completed");
-
-            // Define the new object key for the PDF
-            String pdfKey = objectKey.replace(".txt", ".pdf");
-             pdfKey = pdfKey.replace("input/", "output/");
-            context.getLogger().log("Uploading the PDF to S3: " + pdfKey);
-
-            // Upload the PDF back to S3 with content length
-            byte[] pdfBytes = pdfOutputStream.toByteArray();
-            ByteArrayInputStream pdfInputStream = new ByteArrayInputStream(pdfBytes);
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(pdfBytes.length);
-            metadata.setContentType("application/pdf");
-            s3Client.putObject(bucketName, pdfKey, pdfInputStream, metadata);
-
-            context.getLogger().log("PDF uploaded to S3 successfully");
-
-            response.put("statusCode", 200);
-            response.put("body", "File uploaded successfully");
-        } catch (IOException | DocumentException e) {
-            context.getLogger().log("Error during PDF conversion or upload: " + e.getMessage());
-            response.put("statusCode", 500);
-            response.put("body", "Error: " + e.getMessage());
-        } catch (Exception e) {
-            context.getLogger().log("General error: " + e.getMessage());
-            response.put("statusCode", 400);
-            response.put("body", "Error: " + e.getMessage());
-        }
-        return response;
-    }
-
-    public Map<String, Object> updateThePDFFile(String outObject, Context context, String bucketName, String objectKey,
-             Map<String, Object> response) {
-        try {
-
-            context.getLogger().log("inside update pdf method");
-
-            // Reading the text file content from s3
-            S3Object Texts3Objects = s3Client.getObject(bucketName, objectKey);
-            S3ObjectInputStream s3InputStreams = Texts3Objects.getObjectContent();
-            String TextfileContent = new String(s3InputStreams.readAllBytes(), StandardCharsets.UTF_8);
-            context.getLogger().log("Text File content: " + TextfileContent);
-
-            // Downloading and Reading the PDF file content from s3
-            S3Object PDFs3Object = s3Client.getObject(bucketName, outObject);
-            S3ObjectInputStream s3InputStream = PDFs3Object.getObjectContent();
-
-            // Updating the PDF content
-            context.getLogger().log("Updating pdf content " + new String(s3InputStream.readAllBytes(), StandardCharsets.UTF_8));
-            PdfReader pdfReader = new PdfReader(s3InputStream);
-            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
-            Document document = new Document();
-            PdfCopy copy = new PdfCopy(document, pdfOutputStream);
-            document.open();
-            copy.addDocument(pdfReader);
-            document.newPage();
-            for (String content : TextfileContent.split("\n")) {
-                document.add(new Paragraph(content));
-            }
-            document.close();
-            copy.close();
-            pdfReader.close();
-
-            context.getLogger().log("PDF content update completed");
-
-             // Extract and log the content of the merged PDF
-            PdfReader mergedPdfReader = new PdfReader(new ByteArrayInputStream(pdfOutputStream.toByteArray()));
-            StringBuilder pdfContent = new StringBuilder();
-            for (int i = 1; i <= mergedPdfReader.getNumberOfPages(); i++) {
-                pdfContent.append(PdfTextExtractor.getTextFromPage(mergedPdfReader, i));
-            }
-            mergedPdfReader.close();
-            context.getLogger().log("Merged PDF content: " + pdfContent.toString());
-
-            context.getLogger().log("PDF content update completed and uploading pdf to s3" );
-
-            // Uploading the updated PDF to S3
-            byte[] pdfBytes = pdfOutputStream.toByteArray();
-            ByteArrayInputStream pdfInputStream = new ByteArrayInputStream(pdfBytes);
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(pdfBytes.length);
-            metadata.setContentType("application/pdf");
-            s3Client.putObject(bucketName, outObject, pdfInputStream, metadata);
-            response.put("statusCode", 200);
-            response.put("body", "File updated and uploaded successfully"+pdfOutputStream.toByteArray().toString());
-        } catch (IOException | DocumentException e) {
-            context.getLogger().log("Error during PDF conversion or upload from update pdf: " + e.getMessage());
-            response.put("statusCode", 500);
-            response.put("body", "Error from update pdf : " + e.getMessage());
-        } catch (Exception e) {
-            context.getLogger().log("General error from update pdf: " + e.getMessage());
-            response.put("statusCode", 400);
-            response.put("body", "Error from update pdf: " + e.getMessage());
-        }
-        return response;
+        return "HTTP request sent to Elastic Beanstalk application";
     }
 }
